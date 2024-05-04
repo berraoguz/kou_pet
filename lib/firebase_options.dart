efdb151776e46d4e699d0f46994eafda92159b0a
berraoguz <berraoguz@outlook.com>
2024-05-04 13:47:42 +0300

    updated

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -64,7 +64,7 @@ class DefaultFirebaseOptions {
     messagingSenderId: '862467341877',
     projectId: 'koupet-6c795',
     storageBucket: 'koupet-6c795.appspot.com',
-    iosBundleId: 'com.example.softpati',
+    iosBundleId: 'com.example.koupet',
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
@@ -73,7 +73,7 @@ class DefaultFirebaseOptions {
     messagingSenderId: '862467341877',
     projectId: 'koupet-6c795',
     storageBucket: 'koupet-6c795.appspot.com',
-    iosBundleId: 'com.example.softpati',
+    iosBundleId: 'com.example.koupet',
   );
 
   static const FirebaseOptions windows = FirebaseOptions(
